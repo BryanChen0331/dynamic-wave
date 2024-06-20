6ad0250288af987d94155ea1c4c6713abe1b6b19
BryanChen0331 <111680145+BryanChen0331@users.noreply.github.com>
2024-06-20 19:04:52 +0800

    Update game.js

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -65,7 +65,7 @@ document.addEventListener("DOMContentLoaded", function(){
     const CharacterNames = ["健力龍蝦", "海馬騎士", "鼓手海葵", "RAP河豚", "後搖海豚", "DJ章魚"]
 
     async function getBlueRatio(){
-        const response = await fetch("https://dynamicwave/api/blueRatio");
+        const response = await fetch("https://dynamicwave.org/api/blueRatio");
         const data = await response.json();
         return data.blueRatio;
     }
@@ -341,13 +341,13 @@ document.addEventListener("DOMContentLoaded", function(){
     }
 
     async function addCount(){
-        await fetch("https://www.dynamicwave/api/counter", {
+        await fetch("https://dynamicwave.org/api/counter", {
             method: "POST"
         });
     }
 
     async function postData(data){
-        await fetch("https://www.dynamicwave/api/data", {
+        await fetch("https://dynamicwave.org/api/data", {
             method: 'POST',
             headers: {
             'Content-Type': 'application/json'
