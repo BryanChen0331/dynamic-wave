ee52f6541ec0a4581a70f96f02b8037ff86013c7
BryanChen0331 <111680145+BryanChen0331@users.noreply.github.com>
2024-07-05 01:17:50 +0800

    Add files via upload

---
+++ public/javascripts/index.js
@@ -0,0 +1,75 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const $blueScore = document.querySelector("#blue-score");
+  const $yellowScore = document.querySelector("#yellow-score");
+
+  const eventSource = new EventSource("/sse");
+
+  eventSource.onmessage = function (event) {
+    const totals = JSON.parse(event.data);
+    updateTotals(totals);
+  };
+
+  function updateTotals(totals) {
+    if (totals.blueTotal && totals.yellowTotal) {
+      $blueScore.innerHTML = totals.blueTotal;
+      $yellowScore.innerHTML = totals.yellowTotal;
+    }
+  }
+
+  fetch("/api/total-score")
+    .then(response => response.json())
+    .then(updateTotals);
+
+  const $clock = document.querySelector("#clock");
+  const targetDate = new Date("2024-07-06T10:00:00");
+
+  function updateCountdown() {
+    const currentDate = new Date();
+    const timeDifference = targetDate - currentDate;
+
+    if (timeDifference <= 0) {
+      $clock.innerHTML = `0d 0h 0m 0s`;
+    } else {
+      const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+      $clock.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    }
+  }
+
+  setInterval(updateCountdown, 1000);
+
+  const $hamburger = document.querySelector("#hamburger");
+  const $navItems = document.querySelector("#nav-items");
+
+  $hamburger.addEventListener("click", function () {
+    $navItems.classList.toggle("show");
+  });
+
+  const $mainContainer = document.querySelector("#main-container");
+
+  function updateClockFontSize() {
+    const containerWidth = $mainContainer.offsetWidth;
+    const fontSize = containerWidth / 15;
+
+    $clock.style.fontSize = `${fontSize}px`;
+  }
+
+  function updateScoreFontSize() {
+    const containerWidth = $mainContainer.offsetWidth;
+    const fontSize = containerWidth / 10;
+
+    $blueScore.style.fontSize = `${fontSize}px`;
+    $yellowScore.style.fontSize = `${fontSize}px`;
+  }
+
+  updateClockFontSize();
+  updateScoreFontSize();
+
+  window.addEventListener("resize", () => {
+    updateClockFontSize();
+    updateScoreFontSize();
+  });
+});
